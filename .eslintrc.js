c8b4b4be73008f9051223274a98b97addaccf069
Daniel Fridkin <danielfridkin@gmail.com>
2020-04-05 21:42:45 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: 'erb/typescript',
+  rules: {
+    // A temporary hack related to IDE not resolving correct package.json
+    'import/no-extraneous-dependencies': 'off',
+    'no-param-reassign': [2, { props: false }]
+  },
+  settings: {
+    'import/resolver': {
+      // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
+      node: {},
+      webpack: {
+        config: require.resolve('./configs/webpack.config.eslint.js')
+      }
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    }
+  }
+};
