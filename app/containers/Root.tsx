cb13ea644a177c457029689e2cc69c5677607f54
Daniel Fridkin <danielfridkin@gmail.com>
2020-04-06 12:17:05 -0400

    Removed Routes.tsx
    Just yeeting these files into the trash and seeing what breaks

--- app/containers/Root.tsx
+++ app/containers/Root.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'connected-react-router';
+import { Switch, Route } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
 import { History } from 'history';
+import routes from '../constants/routes.json';
 import { Store } from '../store';
-import Routes from '../Routes';
+import HomePage from './HomePage';
+import CounterPage from './CounterPage';
+import TopologyPage from './TopologyPage';
 
 type Props = {
   store: Store;
@@ -14,7 +18,11 @@ type Props = {
 const Root = ({ store, history }: Props) => (
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <Routes />
+      <Switch>
+        <Route path={routes.COUNTER} component={CounterPage} />
+        <Route path={routes.TOPOLOGY} component={TopologyPage} />
+        <Route path={routes.HOME} component={HomePage} />
+      </Switch>
     </ConnectedRouter>
   </Provider>
 );
