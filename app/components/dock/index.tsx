b74b5db5d5e02409b60534fa12e31140d6320d64
Daniel Fridkin <danielfridkin@gmail.com>
2020-04-09 09:03:26 -0400

    Cleaner popovers, I think.

--- app/components/dock/index.tsx
+++ app/components/dock/index.tsx
@@ -21,27 +21,27 @@ const Dock: React.FunctionComponent<Props> = ({ icons }: Props) => {
       className={`${dock} flex flex-row-reverse mb-2 justify-center mx-auto p-1 rounded-lg`}
     >
       {icons.map(({ path, icon: PageIcon, name }, i) => (
-        <Popover text={name} key={name}>
+        <div
+          key={name}
+          // ml and mr are flipped because of row-reverse
+          className={`${i === 0 ? '' : 'mr-1'} ${
+            i === icons.length - 1 ? '' : 'ml-1'
+          }`}
+        >
           <div
-            key={name}
-            // ml and mr are flipped because of row-reverse
-            className={`${i === 0 ? '' : 'mr-1'} ${
-              i === icons.length - 1 ? '' : 'ml-1'
+            className={`m-0 p-2 rounded-lg w-full ${
+              history.location.pathname === path ? active : inactive
             }`}
           >
-            <div
-              className={`m-0 p-2 rounded-lg w-full ${
-                history.location.pathname === path ? active : inactive
-              }`}
-            >
+            <Popover text={name} offset={100}>
               <BigIcon
                 icon={<PageIcon />}
                 onClick={() => history.push(path)}
                 ariaLabel={name}
               />
-            </div>
+            </Popover>
           </div>
-        </Popover>
+        </div>
       ))}
     </div>
   );
--- app/components/popover/index.tsx
+++ app/components/popover/index.tsx
@@ -3,13 +3,21 @@ import { popoverWrapper } from './styles.scss';
 
 type Props = {
   text: string;
+  offset: number;
   children: ReactNode;
 };
 
-const Popover: React.FunctionComponent<Props> = ({ text, children }: Props) => {
+const Popover: React.FunctionComponent<Props> = ({
+  text,
+  offset,
+  children
+}: Props) => {
   return (
     <div className={popoverWrapper}>
-      <span className="p-1 rounded transition-all px-2 duration-200 ease-in-out">
+      <span
+        className="p-1 rounded cursor-default select-none transition-opacity px-2 duration-200 ease-in-out"
+        style={{ top: `-${offset}%` }}
+      >
         {text}
       </span>
       {children}
